5cbec48cfe781d60b27b1a4757be14fb3f0e6231
HobbaZ <zachobba@gmail.com>
2024-01-15 20:19:53 +1100

    updated template to new dependency versions, started development process

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
+import React from "react";
+import Container from "react-bootstrap/Container";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope} from '@fortawesome/free-solid-svg-icons'
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 //Change year automatically
 function year() {
@@ -12,20 +12,26 @@ function year() {
 }
 
 const Footer = () => {
-return (
-  <>
-  <Container className='text-center' fluid>
-  <footer className=' fixed-bottom text-center'>
-      <p>React Website Template, {year()} 
-
-          <a href="https://www.linkedin.com/in/zachary-hobba-52aaa182/"><FontAwesomeIcon className='icon' icon={faLinkedin} /></a>
-          <a href="mailto:zachobba@gmail.com"><FontAwesomeIcon className='icon' icon={faEnvelope} /></a>
-          <a href="https://github.com/HobbaZ"><FontAwesomeIcon className='icon' icon={faGithub} /></a>
-        </p>
-  </footer>
-  </Container>
-  </>
-);
+  return (
+    <>
+      <Container className="text-center" fluid>
+        <footer className=" fixed-bottom text-center">
+          <p>
+            Print Modular, {year()}
+            {/*<a href="https://www.linkedin.com/in/zachary-hobba-52aaa182/">
+              <FontAwesomeIcon className="icon" icon={faLinkedin} />
+            </a>
+            <a href="mailto:zachobba@gmail.com">
+              <FontAwesomeIcon className="icon" icon={faEnvelope} />
+            </a>
+            <a href="https://github.com/HobbaZ">
+              <FontAwesomeIcon className="icon" icon={faGithub} />
+  </a>*/}
+          </p>
+        </footer>
+      </Container>
+    </>
+  );
 };
 
 export default Footer;
---
+++ src/pages/NewProjects.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Container } from "react-bootstrap";
+
+function NewProjects() {
+  return (
+    <>
+      <Container>
+        <h1 className="text-center">New Projects</h1>
+        <p>
+          This 3 page basic website template was made with React and Bootstrap
+        </p>
+      </Container>
+    </>
+  );
+}
+
+export default NewProjects;
